8c8ee46ed9030da6765a2e8daf01b89ae6f6219e
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-02-24 14:09:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+// INITIALIZATION //
+
+const rows = 5;
+const columns = 5;
+
+// STATE //
+
+// FUNCTIONS //
+
+
+
+function createGrid(rows, columns) {
+  console.log('createGrid() was called');
+  for (let i = 0; i < rows; i++) {
+    let row = $('<div>').addClass('row gamerow');
+    $('#gameboard').append(row);
+    for (let j = 0; j < columns; j++) {
+      let tile = $('<div>').addClass('col tile').text('GIF');
+      row.append(tile);
+    }
+  }
+}
+
+// CREATION //
+createGrid(rows, columns);
+
+// EVENTS //
+
+
+
+})
