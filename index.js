963782e58997b75c3886e1cdcee30db2f14216f5
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-02-24 14:37:11 -0700

    add event listener to submit button

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 $(document).ready(function() {
-// INITIALIZATION //
+  // INITIALIZATION //
 
-const rows = 5;
-const columns = 5;
+  const rows = 4;
+  const columns = 5;
 
-// STATE //
+  // STATE //
 
-// FUNCTIONS //
+  // FUNCTIONS //
 
 
 
-function createGrid(rows, columns) {
-  console.log('createGrid() was called');
-  for (let i = 0; i < rows; i++) {
-    let row = $('<div>').addClass('row tile-row');
-    $('#gameboard').append(row);
-    for (let j = 0; j < columns; j++) {
-      let tile = $('<div>').addClass('col tile').text('GIF');
-      row.append(tile);
+  function createGrid(rows, columns) {
+    for (let i = 0; i < rows; i++) {
+      let row = $('<div>').addClass('row tile-row');
+      $('#gameboard').append(row);
+      for (let j = 0; j < columns; j++) {
+        let tile = $('<div>').addClass('col tile').text('GIF');
+        row.append(tile);
+      }
     }
   }
-}
 
-// CREATION //
-createGrid(rows, columns);
+  // CREATION //
+  createGrid(rows, columns);
 
-// EVENTS //
+  // EVENTS //
 
+  $('button').click(function(event) {
+    event.preventDefault();
+    console.log('You clicked the button');
+    let userSearch = $('#search').val();
+    console.log(userSearch);
+
+  })
 
 
 })
