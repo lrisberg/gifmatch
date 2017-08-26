f82d2b2e56d2f40259adf72b7d1ff964a3e61be0
Lauren <lauren.risberg@gmail.com>
2017-08-26 15:27:00 -0600

    show grid of 12 GIFs on screen

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -6,6 +6,13 @@
 
     function MainController() {
       var vm = this;
+
+      let imgUrl = 'https://media.tenor.co/images/3fa2bbcd2ed33a3ace817ac6e33264d5/tenor.gif';
+      vm.images = [
+        [imgUrl, imgUrl, imgUrl, imgUrl],
+        [imgUrl, imgUrl, imgUrl, imgUrl],
+        [imgUrl, imgUrl, imgUrl, imgUrl],
+      ];
     }
 
 })();
