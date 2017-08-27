5ba963890d6121426dc4cca3607a0a9d142cdf45
Lauren <lauren.risberg@gmail.com>
2017-08-27 14:25:23 -0600

    refactor gifs into their own component

--- client/javascripts/components.js
+++ client/javascripts/components.js
@@ -2,10 +2,20 @@
 
   angular
     .module('gifmatchApp')
+
     .component('gmMain', {
       controller: 'MainController',
       controllerAs: 'vm',
       templateUrl: '../views/main.html'
+    })
+
+    .component('gmGif', {
+      bindings: {
+        img: '<'
+      },
+      controller: 'GifController',
+      controllerAs: 'vm',
+      templateUrl: '../views/gif.html'
     });
 
 })();
--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -3,6 +3,7 @@
   angular
     .module('gifmatchApp')
     .controller('MainController', MainController)
+    .controller('GifController', GifController)
 
     function pick6(gifs) {
       let selectedGifs = [];
@@ -18,7 +19,7 @@
       return selectedGifs;
     }
 
-    function duplicateGifs(subset) {
+    function makeRandomPairs(subset) {
       let duplicatedGifs = [];
       for (let i = 0; i < 2; i++) {
         for (let gif of subset) {
@@ -40,15 +41,17 @@
     function MainController() {
       var vm = this;
 
-      let imgUrl = 'https://media.tenor.co/images/3fa2bbcd2ed33a3ace817ac6e33264d5/tenor.gif';
-
       let gifs = ['https://media.tenor.co/images/c4ddaade7b4f2498abf4ac124073c8ae/tenor.gif', 'https://media.tenor.co/images/a7051e31e1633d00a277e72ecef1d550/tenor.gif', 'https://media.tenor.co/images/afbc61d033fa7488107e1cc667893b11/tenor.gif', 'https://media.tenor.co/images/28914c3f5b751bbaf24da3121bc1375b/tenor.gif', 'https://media.tenor.co/images/067ce00fb8fac1831af4ecbdec600ba6/tenor.gif', 'https://media.tenor.co/images/4bf02d0ab25493e338248c084f0efb61/tenor.gif', 'https://media.tenor.co/images/42185f13c396d9dd217bfd1afcc0d021/tenor.gif', 'https://media.tenor.co/images/659bd2d2faf926550b27e4b61b9efebc/tenor.gif', 'https://media.tenor.co/images/a4a5bd805e6532c766a5bbdd3b0e5c07/tenor.gif', 'https://media.tenor.co/images/aa0478e5e16d0293e0952520903b0161/tenor.gif']
 
       let subset = pick6(gifs); // [1, 2, 3]
-      let subsetWithDupes = duplicateGifs(subset); // [2, 3, 2, 1, 1, 3]
+      let subsetWithDupes = makeRandomPairs(subset); // [2, 3, 2, 1, 1, 3]
       let gridOfGifs = griddify(subsetWithDupes); // [[:4], [:4], [:4]]
 
       vm.images = gridOfGifs;
+    }
+
+    function GifController() {
+      let vm = this;
     }
 
 })();
