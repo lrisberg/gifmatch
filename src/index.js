2063952c1dbdd696c945fb75833b6337aab3cb18
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-10-11 09:50:01 -0600

    renders grid from array of gifs

--- src/index.js
+++ src/index.js
@@ -3,54 +3,65 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Tile extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
-      <button className="tile">
-      </button>
+      <img src={this.props.gif} className="tile">
+      </img>
     );
   }
 }
 
 class Board extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      height: 5,
-      length: 4
-    }
+    super(props)
   }
 
-  renderTile(row, column) {
-    const key = `${row}, ${column}`
-    return <Tile key={key} />;
+  renderTile(gif) {
+    return <Tile gif={gif} />;
   }
 
-  renderRow(row, tiles) {
+  renderRow(tiles) {
     return (
-      <div key={row} className="board-row">
+      <div className="board-row">
         {tiles}
       </div>
     )
   }
 
-  renderGrid(length, height) {
+  renderGrid(gifs) {
     let rows = [];
-    for (let i = 0; i < height; i++) {
+    for (let i = 0; i < gifs.length; i++) {
       let tiles = [];
-      for (let j = 0; j < length; j++) {
-        tiles.push(this.renderTile(i, j));
+      for (let j = 0; j < gifs[i].length; j++) {
+        tiles.push(this.renderTile(gifs[i][j]))
       }
-      const row = this.renderRow(i, tiles);
+      const row = this.renderRow(tiles);
       rows.push(row);
     }
     return rows;
+
+
+    // let rows = [];
+    // for (let i = 0; i < 4; i++) {
+    //   let tiles = [];
+    //   for (let j = 0; j < 4; j++) {
+    //     tiles.push(this.renderTile(i, j));
+    //   }
+    //   const row = this.renderRow(i, tiles);
+    //   rows.push(row);
+    // }
+    // return rows;
   }
 
   render() {
     return (
       <div>
         <div className="board-row">
-          {this.renderGrid(this.state.length, this.state.height)}
+          {this.renderGrid(this.props.gifs)}
         </div>
       </div>
     );
@@ -58,11 +69,46 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: 5,
+      length: 4,
+      gifs:
+      [
+        [
+          'https://media.giphy.com/media/d1FL4zXfIQZMWFQQ/giphy.gif',
+          'https://media.giphy.com/media/d1FL4zXfIQZMWFQQ/giphy.gif',
+          'https://media.giphy.com/media/l3vRjYearYzgNb7e8/giphy.gif',
+          'https://media.giphy.com/media/l3vRjYearYzgNb7e8/giphy.gif'
+        ],
+        [
+          'https://media.giphy.com/media/l3q2yYNt8DXoyKRdm/giphy.gif',
+          'https://media.giphy.com/media/l3q2y9WQRuooRmyfS/giphy.gif',
+          'https://media.giphy.com/media/l3q2tvaYp4KseN22A/giphy.gif',
+          'https://media.giphy.com/media/l3q2tvaYp4KseN22A/giphy.gif'
+        ],
+        [
+          'https://media.giphy.com/media/l3q2L3yM5UhxEnsvC/giphy.gif',
+          'https://media.giphy.com/media/3oz8xDzuVDbKoU4shi/giphy.gif',
+          'https://media.giphy.com/media/26xBy4g1eHS1vqZRS/giphy.gif',
+          'https://media.giphy.com/media/26xBy4g1eHS1vqZRS/giphy.gif'
+        ],
+        [
+          'https://media.giphy.com/media/3o6ZsYZtTSdFOdxPEY/giphy.gif',
+          'https://media.giphy.com/media/3o6ZsYZtTSdFOdxPEY/giphy.gif',
+          'https://media.giphy.com/media/3o6ZsYZtTSdFOdxPEY/giphy.gif',
+          'https://media.giphy.com/media/3o6ZsYZtTSdFOdxPEY/giphy.gif'
+        ]
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board gifs={this.state.gifs}/>
         </div>
       </div>
     );
