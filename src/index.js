50cc5cc9e3171f3fd4799d04c2a524bcf7dff992
Lauren Risberg <lauren.risberg@kapost.com>
2017-11-05 10:30:51 -0700

    gifs are shuffled every time

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import './index.css';
 
 class Tile extends React.Component {
@@ -35,12 +36,16 @@ class Board extends React.Component {
     super(props);
     this.state = {
       currentGif: null,
+      waiting: false,
       tileVisibility: {
       }
     }
   }
 
   onSelectGif(gif, key) {
+    if (this.state.waiting) {
+      return;
+    }
     const visibility = this.state.tileVisibility;
     visibility[key] = true;
     this.setState({tileVisibility: visibility});
@@ -58,11 +63,15 @@ class Board extends React.Component {
       else {
         console.log('not a match');
         const currentKey = this.state.currentKey;
+        this.setState({
+          waiting: true
+        })
         setTimeout(() => {
           visibility[key] = false;
           visibility[currentKey] = false;
           this.setState({
-            tileVisibility: visibility
+            tileVisibility: visibility,
+            waiting: false
           })
         }, 500)
       }
@@ -115,33 +124,22 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props)
+    const sourceGifs = [
+      'https://media.giphy.com/media/d1FL4zXfIQZMWFQQ/giphy.gif',
+      'https://media.giphy.com/media/l3q2Fa0XM2SEciHaU/giphy.gif',
+      'https://media.giphy.com/media/26xBy4g1eHS1vqZRS/giphy.gif',
+      'https://media.giphy.com/media/l3vRjYearYzgNb7e8/giphy.gif',
+      'https://media.giphy.com/media/l3q2yYNt8DXoyKRdm/giphy.gif',
+      'https://media.giphy.com/media/l3q2y9WQRuooRmyfS/giphy.gif',
+      'https://media.giphy.com/media/l3q2L3yM5UhxEnsvC/giphy.gif',
+      'https://media.giphy.com/media/3oz8xDzuVDbKoU4shi/giphy.gif'
+    ];
+    let sourceGifsDoubled = _.concat(sourceGifs, sourceGifs)
+    let shuffledGifs = _.shuffle(sourceGifsDoubled);
+    let groupedGifs = _.chunk(shuffledGifs, 4);
+
     this.state = {
-      gifs: [
-        [
-          'https://media.giphy.com/media/d1FL4zXfIQZMWFQQ/giphy.gif',
-          'https://media.giphy.com/media/l3q2Fa0XM2SEciHaU/giphy.gif',
-          'https://media.giphy.com/media/26xBy4g1eHS1vqZRS/giphy.gif',
-          'https://media.giphy.com/media/l3vRjYearYzgNb7e8/giphy.gif'
-        ],
-        [
-          'https://media.giphy.com/media/l3q2yYNt8DXoyKRdm/giphy.gif',
-          'https://media.giphy.com/media/l3q2y9WQRuooRmyfS/giphy.gif',
-          'https://media.giphy.com/media/d1FL4zXfIQZMWFQQ/giphy.gif',
-          'https://media.giphy.com/media/l3q2Fa0XM2SEciHaU/giphy.gif'
-        ],
-        [
-          'https://media.giphy.com/media/l3q2L3yM5UhxEnsvC/giphy.gif',
-          'https://media.giphy.com/media/3oz8xDzuVDbKoU4shi/giphy.gif',
-          'https://media.giphy.com/media/l3vRjYearYzgNb7e8/giphy.gif',
-          'https://media.giphy.com/media/26xBy4g1eHS1vqZRS/giphy.gif'
-        ],
-        [
-          'https://media.giphy.com/media/l3q2L3yM5UhxEnsvC/giphy.gif',
-          'https://media.giphy.com/media/l3q2yYNt8DXoyKRdm/giphy.gif',
-          'https://media.giphy.com/media/l3q2y9WQRuooRmyfS/giphy.gif',
-          'https://media.giphy.com/media/3oz8xDzuVDbKoU4shi/giphy.gif'
-        ]
-      ]
+      gifs: groupedGifs
     }
   }
 
